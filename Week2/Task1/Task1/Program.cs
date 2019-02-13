42820bd2a5d36a51211df96d2a55366c62a26f2d
darbayenurdauletsmelyi <darbayevn@gmail.com>
2019-02-13 16:49:44 +0600

    Add Week2

---
+++ Week2/Task1/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Task_1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadToEnd();
+            char[] ch = s.ToArray();
+            Array.Reverse(ch);
+            string reverse = String.Empty;
+            for (int i = 0; i < ch.Length; i++)
+            {
+                reverse += ch[i];
+            }
+            if (s != reverse)
+            {
+                Console.WriteLine("No");
+
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week2/Task4/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathString = @"C:\Users\Nurdaulet\Desktop\papka\path";
+            string fileName = "Nurda.txt";
+            if (!System.IO.File.Exists(pathString))
+            {
+                using (System.IO.FileStream fs = System.IO.File.Create(pathString))//сделать через стрим врайтер и копи делет через файл 
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fs.WriteByte(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File \"{0}\" already exists.", fileName);
+                return;
+            }
+            string destFile = "C:/Users/Nurdaulet/Desktop/papka/path/Nurda.txt";
+            string sourceFile = "C:/Users/Nurdaulet/Desktop/papka/path1/Nurda.txt";
+            System.IO.File.Copy(sourceFile, destFile, true);
+            if (System.IO.File.Exists("C:/Users/Nurdaulet/Desktop/papka/path/Nurda.txt"))
+            {
+
+                try
+                {
+                    System.IO.File.Delete("C:/Users/Nurdaulet/Desktop/papka/path/Nurda.txt");
+                }
+                catch (System.IO.IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+            }
+
+        }
+    }
+}
