05f4fafd30622dd83c889c59018d622840c1cc8b
darbayenurdauletsmelyi <darbayevn@mail.com>
2019-03-30 13:14:56 +0600

    week 4 and mid

---
+++ factorial and sum/Task1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = int.Parse(Console.ReadLine());
+            Number n = new Number(a);
+            n.Start(n.N);
+        }
+    }
+}
+    public class Number
+    {
+        public int N;
+        public Number(int N)
+        {
+            this.N = N;
+        }
+        public void Start(int N)
+        {
+
+            Thread th = new Thread(Wark);
+            Thread th2 = new Thread(Work);
+            th.Start();
+            th2.Start();
+
+
+        }
+
+        public void Wark()
+        {
+            while (true)
+            {
+                Factorial();
+                Thread.Sleep(700);
+            }
+        }
+        public void Work()
+        {
+            while (true)
+            {
+
+                Sum();
+                Thread.Sleep(700);
+
+            }
+
+        }
+        public void Sum()
+        {
+            int ann = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                ann += i;
+            }
+            Console.WriteLine("Sum of " + N + " is :" + ann);
+
+
+        }
+
+        public void Factorial()
+        {
+
+            int ans = 1;
+            for (int i = 2; i <= N; i++)
+                ans *= i;
+            Console.WriteLine("Factorial of " + N + " is :" + ans);
+        }
+    }
+
+
